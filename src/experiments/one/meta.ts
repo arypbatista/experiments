ac971867217add91054df52c14629b89c8f43743
Ary Pablo Batista <arypbatista@gmail.com>
2026-04-07 21:25:07 -0300

    Add 'one' experiment: Creation of Adam as WebGL dot-matrix shader

---
+++ src/experiments/one/meta.ts
@@ -0,0 +1,10 @@
+import type { ExperimentMeta } from '../types';
+
+const meta: ExperimentMeta = {
+  title: 'One',
+  description: 'The Creation of Adam rendered as a field of white dots using a WebGL fragment shader.',
+  tags: ['webgl', 'shader', 'dots'],
+  date: '2026-04-07',
+};
+
+export default meta;
