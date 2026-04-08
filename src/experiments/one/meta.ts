a4e2ee8e6837e758e6b877b93985d10de7e973e0
Ary Pablo Batista <arypbatista@gmail.com>
2026-04-07 22:44:53 -0300

    Rename assets and add thumbnail to 'one' experiment

--- src/experiments/one/meta.ts
+++ src/experiments/one/meta.ts
@@ -1,10 +1,12 @@
 import type { ExperimentMeta } from '../types';
+import thumbnail from './thumbnail.png';
 
 const meta: ExperimentMeta = {
   title: 'One',
-  description: 'The Creation of Adam rendered as a field of white dots using a WebGL fragment shader.',
-  tags: ['webgl', 'shader', 'dots'],
+  description: 'The Creation of Adam as an interactive Three.js point cloud with a bubble effect.',
+  tags: ['three.js', 'points', 'interactive'],
   date: '2026-04-07',
+  thumbnail,
 };
 
 export default meta;
